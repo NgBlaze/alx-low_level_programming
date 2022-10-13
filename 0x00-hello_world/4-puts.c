908186489713446153ac0a97f3af602b0aef39c2
NgBlaze <okolaaprecious@gmail.com>
2022-10-13 07:40:46 -0700

    created a c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/* This is a function that prints a stateement */
+	char str[] = "Programming is like building a multilingual puzzle";
+	puts(str);
+	return (0);
+}
+
