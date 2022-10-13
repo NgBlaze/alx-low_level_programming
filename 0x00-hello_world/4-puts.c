eb6a77a1ca3780568602fb7f7a8786741600b6cd
NgBlaze <okolaaprecious@gmail.com>
2022-10-13 07:59:50 -0700

    Updated Script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+/* This is a function that prints a line*/
+
 int main(void)
 {
-/* This is a function that prints a stateement */
 
 char str[] = "\"Programming is like building a multilingual puzzle";
 
