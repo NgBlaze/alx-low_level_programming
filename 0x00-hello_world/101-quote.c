42170c1c25e6a6b29ca9cc99410949fd2997f91a
NgBlaze <okolaaprecious@gmail.com>
2022-10-13 15:08:44 -0700

    Created C sccript

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Short description, single line
+ * a blank line
+ * Return: Description of the returned value
+ */
+
+int main(void)
+{
+	printf("%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
