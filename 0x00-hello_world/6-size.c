b02027ac4b586c6f64ff37ec37f14f6eea34944d
NgBlaze <okolaaprecious@gmail.com>
2022-10-13 13:35:55 -0700

    Created c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Short description, single line
+ * a blank line
+ * Description: Longer description of the function)?
+ * Return: Description of the returned value
+ */
+
+int main(void)
+{
+	int integerType;
+
+	float floatType;
+
+	double doubleType;
+
+	char charType
+	/* Sizeof operator is used to evaluate the size of a variable */
+	printf("Size of int: %ld bytes\n", sizeof(integerType));
+	printf("Size of float: %ld bytes\n", sizeof(floatType));
+	printf("Size of double: %ld bytes\n", sizeof(doubleType));
+	printf("Size of char: %ld byte\n", sizeof(charType));
+
+	return (0);
+}
