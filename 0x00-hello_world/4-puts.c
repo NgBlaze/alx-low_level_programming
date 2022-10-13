6ae1d334636ff9a1446f6f30c53467001acf2872
NgBlaze <okolaaprecious@gmail.com>
2022-10-13 08:34:51 -0700

    Updated Script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 /**
- *  * A function that prints hello world
- *   */
+ *  * main - Short description, single line
+ *  (* a blank line
+ *   * Description: Longer description of the function)?
+ *    * Return: Description of the returned value
+ *     */
 
 int main(void)
 {
-
-char str[] = "\"Programming is like building a multilingual puzzle";
-
-puts(str);
-return (0);
+	        /*Print sre */
+	        char str[] = "\"Programming is like building a multilingual puzzle";
+		        /* Print String */
+		        puts(str);
+			        return (0);
 }
+
