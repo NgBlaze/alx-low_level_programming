51c87e2b348ff87a777303eed5bca9c691d62c47
NgBlaze <okolaaprecious@gmail.com>
2022-10-13 07:56:46 -0700

    ss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 
 int main(void)
 {
-	/* This is a function that prints a stateement */
-	char str[] = "Programming is like building a multilingual puzzle";
-	puts(str);
-	return (0);
-}
+/* This is a function that prints a stateement */
 
+char str[] = "\"Programming is like building a multilingual puzzle";
+
+puts(str);
+return (0);
+}
