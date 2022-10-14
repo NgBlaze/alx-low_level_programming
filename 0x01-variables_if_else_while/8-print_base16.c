aac85b7169e6b0913eadf606dab4454f7632f450
NgBlaze <okolaaprecious@gmail.com>
2022-10-14 12:12:26 -0700

    Created c file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+/**
+ * main - Short description, single line
+ * a blank line
+ * Description: Longer description of the function)?
+ * Return: Description of the returned value
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+	putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
