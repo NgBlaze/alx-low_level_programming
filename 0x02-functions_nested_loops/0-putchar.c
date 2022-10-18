39948859bfc58b2fbc1ccd4576601a029b718a65
NgBlaze <okolaaprecious@gmail.com>
2022-10-18 07:11:16 -0700

    created c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - Short description
+ * a blank line
+ * Description: A function that prints a string
+ * Return: Description of the returned value
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
