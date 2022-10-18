26c98182fa00a2a36715c752145be2b166adc91b
NgBlaze <okolaaprecious@gmail.com>
2022-10-18 08:56:39 -0700

    Updated files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
-
+#include "main.h"
 
 /**
- * main - Short description
- * a blank line
+ * putstr - Print a String
+ *
  * Description: A function that prints a string
- * Return: Description of the returned value
+ * Return: Returns 0
  */
+
+
+int putstr(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+
+}
+
+/**
+ * main - Calls the Function
+ *
+ * Description: A function that prints a string
+ * Return: Returns 0
+ */
+
 
 int main(void)
 {
-	printf("_putchar\n");
+	int ret;
+	/* function call */
+	ret = putstr();
 
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,1 @@
+int putstr(void);
